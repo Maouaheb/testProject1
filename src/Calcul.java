65304837af6695fcd7d6fff502331c8e70cb1cb6
Maweheb Belarbi <mawbelmawbel@gmail.com>
2020-10-15 09:51:46 +0200

    ajout d'une classe Calcul.java

---
+++ src/Calcul.java
@@ -0,0 +1,9 @@
+
+public class Calcul {
+public int Sum(int a, int b) {
+return a+b;	
+}
+public int multiplication(int a, int b) {
+	return a*b;
+}
+}
--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Hello {
 public static void main(String[] args) {
-	int i=2;
-	System.out.println(i);
+	Scanner scanner1=new Scanner(System.in);
+    System.out.print("Enter first number: ");
+    int a=scanner1.nextInt();
+    Scanner scanner2=new Scanner(System.in);
+    System.out.print("Enter second number: ");
+    int b=scanner2.nextInt();
+    Calcul calcul=new Calcul();
+    System.out.println("résultat d'addition de "+a+" + "+b+" = "+calcul.Sum(a, b));
+    System.out.println("résultat multiplication de "+a+" * "+b+" = "+calcul.multiplication(a, b));
+
+    
 	
 }
 }
