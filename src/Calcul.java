f55921bdfe17d862a120762c9700cca2690d89af
Maouaheb <37180996+Maouaheb@users.noreply.github.com>
2020-10-15 09:59:22 +0200

    Update Calcul.java

--- src/Calcul.java
+++ src/Calcul.java
@@ -6,4 +6,11 @@ return a+b;
 public int multiplication(int a, int b) {
 	return a*b;
 }
+public int puissance(int a, int b) {
+	int puissance=1;
+	for(int i=1;i<=b;i++){
+	puissance=a*a;
+	}
+	return puissance;
+}
 }
